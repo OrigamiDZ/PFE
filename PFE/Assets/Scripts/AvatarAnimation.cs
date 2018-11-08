59672b0fe325e8be8b940676230087e5dfa6b97d
louis <lpdt@live.fr>
2018-11-08 14:43:35 +0100

    start script bihou

---
+++ PFE/Assets/Scripts/AvatarAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AvatarActions
+{
+    /*looping,
+    joke,
+    fearAnimation,
+    happyAnimation,*/
+    joyAnimation,
+    none
+};
+public class AvatarAnimation : MonoBehaviour {
+
+    private static AvatarActions avatarCurrentAction = AvatarActions.none;
+
+    public AvatarActions AvatarCurrentAction {
+        get {
+            return avatarCurrentAction;
+        }
+
+        set {
+            avatarCurrentAction = value;
+        }
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
