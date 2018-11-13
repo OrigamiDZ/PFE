915d03d9c88a484a7e83cdcffb84299a1a94e759
LoW <lea-saunier@hotmail.fr>
2018-11-13 11:59:28 +0100

    Image recognition works

--- ARCore/Assets/Lea/Script/SceneManagerCanvas.cs
+++ ARCore/Assets/Lea/Script/SceneManagerCanvas.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneManagerCanvas : MonoBehaviour {
 
+    public Text sceneNameText;
+
+    private void Start()
+    {
+        Debug.Log("scene count = " + SceneManager.sceneCountInBuildSettings);
+        Debug.Log("previous scene = " + (SceneManager.GetActiveScene().buildIndex - 1) % SceneManager.sceneCountInBuildSettings);
+        Debug.Log("next scene = " + (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+
+        switch (SceneManager.GetActiveScene().buildIndex)
+        {
+            case 0:
+                sceneNameText.text = "Hello AR";
+                Debug.Log("Scene = " + SceneManager.GetActiveScene().buildIndex);
+                break;
+
+            case 1:
+                sceneNameText.text = "Object in Front Camera";
+                Debug.Log("Scene = " + SceneManager.GetActiveScene().buildIndex);
+                break;
+
+            case 2:
+                sceneNameText.text = "Image recognition";
+                Debug.Log("Scene = " + SceneManager.GetActiveScene().buildIndex);
+                break;
+        }
+    }
+
     public void NextSceneButton()
     {
-        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCount);
+        Debug.Log("next scene = " + (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+        Debug.Log("scene count = " + SceneManager.sceneCountInBuildSettings);
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
     }
 
     public void PreviousSceneButton()
     {
-        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex - 1) % SceneManager.sceneCount);
+        Debug.Log("previous scene = " + (SceneManager.GetActiveScene().buildIndex - 1) % SceneManager.sceneCountInBuildSettings);
+        Debug.Log("scene count = " + SceneManager.sceneCountInBuildSettings);
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex - 1) % SceneManager.sceneCountInBuildSettings);
     }
 
 }
