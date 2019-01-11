e3c544d97ccc04448c9ca3f54fc5d10021005d2f
Lea Saunier <lea-saunier@hotmail.fr>
2019-01-11 12:11:47 +0100

    Tuto slides beginning

---
+++ PFE/Assets/Scripts/Tuto_UImanager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tuto_UImanager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject[] tutoSlides;
+
+    private GameObject currentSlide;
+    private int currentSlideId;
+
+    private void Start()
+    {
+        if(tutoSlides.Length > 0)
+        {
+            currentSlide = tutoSlides[0];
+            currentSlideId = 0;
+            currentSlide.SetActive(true);
+        }
+    }
+
+    public void OnClickNextButton()
+    {
+        
+    }
+}
