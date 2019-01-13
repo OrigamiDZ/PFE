bd3d13145074020b1b4fc86ef64c51053d2d6cd5
Lea Saunier <lea-saunier@hotmail.fr>
2019-01-13 17:14:19 +0100

    Adding AR assets + tuto

--- PFE/Assets/Scripts/Tuto_UImanager.cs
+++ PFE/Assets/Scripts/Tuto_UImanager.cs
@@ -7,6 +7,9 @@ public class Tuto_UImanager : MonoBehaviour {
 
     [SerializeField]
     GameObject[] tutoSlides;
+    [SerializeField]
+    GameObject testingPhaseGameobject;
+
 
     private GameObject currentSlide;
     private int currentSlideId;
@@ -23,6 +26,29 @@ public class Tuto_UImanager : MonoBehaviour {
 
     public void OnClickNextButton()
     {
-        
+        if(currentSlideId < tutoSlides.Length - 1)
+        {
+            currentSlide.SetActive(false);
+            currentSlideId++;
+            currentSlide = tutoSlides[currentSlideId];
+            currentSlide.SetActive(true);
+        }
+    }
+
+    public void OnClickPreviousButton()
+    {
+        if (currentSlideId > 0)
+        {
+            currentSlide.SetActive(false);
+            currentSlideId--;
+            currentSlide = tutoSlides[currentSlideId];
+            currentSlide.SetActive(true);
+        }
+    }
+
+    public void OnClickStartTestingPhaseButton()
+    {
+        currentSlide.SetActive(false);
+        testingPhaseGameobject.SetActive(true);
     }
 }
