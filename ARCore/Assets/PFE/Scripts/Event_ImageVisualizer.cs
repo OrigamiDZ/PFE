a12e33addfeaa48a02e357f1eadc093945f8f180
Lea Saunier <lea-saunier@hotmail.fr>
2019-01-23 13:53:45 +0100

    Event oh yeeeeeeeeeaaaaah

---
+++ ARCore/Assets/PFE/Scripts/Event_ImageVisualizer.cs
@@ -0,0 +1,44 @@
+﻿namespace GoogleARCore.Examples.AugmentedImage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using GoogleARCore;
+    using GoogleARCoreInternal;
+    using UnityEngine;
+
+    /// <summary>
+    /// Uses 4 frame corner objects to visualize an AugmentedImage.
+    /// </summary>
+    public class Event_ImageVisualizer : MonoBehaviour
+    {
+        /// <summary>
+        /// The AugmentedImage to visualize.
+        /// </summary>
+        public AugmentedImage Image;
+
+        /// <summary>
+        /// An object to place when an image is detected.
+        /// </summary>
+        public GameObject AugmentedObject;
+
+        /// <summary>
+        /// The Unity Update method.
+        /// </summary>
+        public void Update()
+        {
+            if (Image == null || Image.TrackingState != TrackingState.Tracking)
+            {
+                AugmentedObject.SetActive(false);
+
+                return;
+            }
+
+            //float halfWidth = Image.ExtentX / 2;
+            //float halfHeight = Image.ExtentZ / 2;
+            //Egg.transform.localPosition = (halfWidth * Vector3.left) + (halfHeight *Vector3.back);
+
+            AugmentedObject.SetActive(true);
+        }
+    }
+}
