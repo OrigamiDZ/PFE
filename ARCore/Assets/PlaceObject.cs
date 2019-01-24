af681d92e9c5b3839f438b508ce0cf772f7a2566
Lea Saunier <lea-saunier@hotmail.fr>
2019-01-24 10:11:54 +0100

    Adding LocationScene

--- ARCore/Assets/PlaceObject.cs
+++ ARCore/Assets/PlaceObject.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using Mapbox.Unity.Location;
+using UnityEngine.SceneManagement;
 
 public class PlaceObject : MonoBehaviour {
 
-
+    GameObject player;
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void LateUpdate()
@@ -13,6 +15,13 @@ public class PlaceObject : MonoBehaviour {
         var map = LocationProviderFactory.Instance.mapManager;
         Mapbox.Utils.Vector2d gymnaseCoord = new Mapbox.Utils.Vector2d(48.625843, 2.44165);
         transform.localPosition = map.GeoToWorldPosition(gymnaseCoord); //48.625843, 2.44165
+
+        float distance = Mathf.Sqrt(Mathf.Pow(player.transform.position.x - transform.localPosition.x, 2) + Mathf.Pow(player.transform.position.x - transform.localPosition.x, 2));
+
+        if (distance < 10)
+        {
+            SceneManager.LoadScene("MiniGame_hidenseek");
+        }
     }
     
 }
