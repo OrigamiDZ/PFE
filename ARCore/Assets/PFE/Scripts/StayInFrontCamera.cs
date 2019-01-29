139ac751c853bd8d4fc3cd22df122525136bb3ac
Lea Saunier <lea-saunier@hotmail.fr>
2019-01-29 11:33:28 +0100

    Controller event OK

---
+++ ARCore/Assets/PFE/Scripts/StayInFrontCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayInFrontCamera : MonoBehaviour
+{
+
+    public Camera FirstPersonCamera;
+
+    public float distToCamera;
+
+
+    private Vector3 positionObjectInFrontCamera;
+
+
+
+
+    void Update()
+    {
+        positionObjectInFrontCamera = FirstPersonCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 50, distToCamera));
+        transform.position = positionObjectInFrontCamera;
+        transform.LookAt(FirstPersonCamera.transform);
+    }
+}
